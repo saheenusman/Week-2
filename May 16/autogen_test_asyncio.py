e4d69f2debc4ff2b124b9ffb1fa69e87ecac8ea8
saheenusman <94819307+saheenusman@users.noreply.github.com>
2025-05-17 17:15:02 +0530

    Update autogen_test_asyncio.py

--- May 16/autogen_test_asyncio.py
+++ May 16/autogen_test_asyncio.py
@@ -4,7 +4,7 @@ import asyncio
 config_list = [
     {
         'model': 'gemini-2.0-flash',
-        'api_key': 'AIzaSyDDaUTEWZGkEvfT46SVH_qOs_QPQJcHLsg',  # Replace with your actual API key
+        'api_key': '',  # Replace with your actual API key
         "api_type": "google",
     }
 ]
